5315cdaed1683d2d7eaac1dc1a923b8ef66ebc40
vivek8091 <72376007+vivek8091@users.noreply.github.com>
2025-03-10 12:27:51 +0530

    Add gallary page completed

--- src/Components/AddGallary.js
+++ src/Components/AddGallary.js
@@ -1,10 +1,34 @@
-function AddGallary(){
-    return(
-        <>
-            AddGallary
-        </>
-    );
-}
+function AddGallary() {
+  return (
+    <>
+      <div className="event-title mt-3">
+        <div className="event-logo">
+          <i class="fa-solid fa-images"></i>
+        </div>
+        <h5>Add Gallary</h5>
+      </div>
 
+      <div className="add-gallary">
+        <div className="mb-4 file-input-container">
+          <input type="file" className="file-input" id="file" />
+          <label for="file" class="custom-file-label">
+            <span class="choose-btn">Choose an Image</span>
+          </label>
+        </div>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Gallary Title"
+            className="form-control"
+          />
+        </div>
+
+        <button type="submit" class="btn btn-primary w-100">
+          Post
+        </button>
+      </div>
+    </>
+  );
+}
 
 export default AddGallary;
