d995e0281db2b9a5fc0f1b6b8ab2aad63505c535
vivek8091 <72376007+vivek8091@users.noreply.github.com>
2025-04-25 11:33:24 +0530

    Admin contact list and login configuration completed...

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,44 +1,24 @@
-const contactData = [
-    {
-        id: 1,
-        name: "vishal",
-        email: "vishal@gmail.com",
-        phone: 998877665515,
-        message: "hello admin!!!",
-    },
-    {
-        id: 2,
-        name: "nayan",
-        email: "nayan@gmail.com",
-        phone: 998877663423,
-        message: "new user!!!",
-    },
-    {
-        id: 3,
-        name: "krunal",
-        email: "krunal@gmail.com",
-        phone: 998877356655,
-        message: "hii...",
-    },
-    {
-        id: 4,
-        name: "kaushik",
-        email: "kushik@gmail.com",
-        phone: 998877665455,
-        message: "new user!!!",
-    },
-    {
-        id: 1,
-        name: "kartik",
-        email: "kartik@gmail.com",
-        phone: 998877667555,
-        message: "hello admin!!!",
-    },
-];
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 function ContactList() {
+
+  const [contactList, setContactList] = useState([]);
+  useEffect(() => {
+    fetchContacts();
+  }, []);
+
+  const fetchContacts = async (e) => {
+    try {
+      const res = await axios.get("http://localhost:2121/api/contact/getContact/");
+      setContactList(res.data.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error("Error while fetching contact details!!!", error);
+      alert("Could not get contact data!!!");
+    }
+  };
   return (
     <>
       <div className="contact-table ">
@@ -49,17 +29,19 @@ function ContactList() {
                 <th>No.</th>
                 <th>Name</th>
                 <th>Email</th>
+                <th>Country</th>
                 <th>Contact No.</th>
                 <th>Message</th>
               </tr>
             </thead>
             <tbody>
-              {contactData.map((contact, index) => (
+              {contactList.map((contact, index) => (
                 <tr key={contact.id} className="align-middle">
                   <td>{index + 1}</td>
                   <td>{contact.name}</td>
                   <td>{contact.email}</td>
-                  <td>{contact.phone}</td>
+                  <td>{contact.country}</td>
+                  <td>{contact.phone_no}</td>
                   <td>{contact.message}</td>
                 </tr>
               ))}
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,26 +1,60 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+  const navigate = useNavigate();
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:2121/api/admin/adminLogin/", loginData);
+      localStorage.setItem("admin", JSON.stringify(res.data.admin));
+      console.log("Logged in admin...", res.data.admin);
+      navigate("/");
+    } catch (error) {
+      
+    }
+  };
   return (
     <>
       <section className="login-section">
         <div className="login-form">
-          <form>
+          <form onSubmit={handleLogin}>
             <div className="mb-3">
-              <label for="email" className="form-label">
+              <label htmlFor="email" className="form-label">
                 Email
               </label>
               <input
                 type="email"
+                name="email"
+                value={loginData.email}
+                onChange={handleChange}
                 className="form-control"
                 id="email"
                 aria-describedby="emailHelp"
               />
             </div>
             <div className="mb-3">
-              <label for="password" className="form-label">
+              <label htmlFor="password" className="form-label">
                 Password
               </label>
               <input
                 type="password"
+                name="password"
+                value={loginData.password}
+                onChange={handleChange}
                 className="form-control"
                 id="password"
               />
@@ -31,7 +65,7 @@ function Login() {
                 className="form-check-input"
                 id="exampleCheck1"
               />
-              <label className="form-check-label" for="exampleCheck1">
+              <label className="form-check-label" htmlFor="exampleCheck1">
                 Remember Me
               </label>
             </div>
